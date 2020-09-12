c2850d3115b84f715f1012cd7833894cd23feeed
Marcelo Ortiz Peredo <a2018119133@estudiantes.upsa.edu.bo>
2020-09-12 11:56:28 -0400

    Creacion del nvc

---
+++ ProductEnum/ProductEnum/Models/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProductEnum.Models
+{
+    public class DataContext: System.Data.Entity.DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<ProductEnum.Models.Product> Products { get; set; }
+    }
+}
